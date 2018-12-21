e3b4f2e3d4d8dc0e36bac6ff5e42b677bdfb3112
CMS Roma DAQ user <cmsroma.daq@gmail.com>
2018-12-21 15:36:53 +0100

    fix to remove temporary files

--- connect_TOFPET.sh
+++ connect_TOFPET.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-file1="/tmp/d.sock"
-file2="/dev/shm/daqd shm"
+fileA="/tmp/d.sock"
+fileB="/dev/shm/daqd_shm"
 
-if [ -f "$file1" ]
+if [ -f "$fileA" ]
 then
-    echo "removing $file1"
-    rm -i $file1
+    echo "removing $fileA"
+    rm -i $fileA
 fi
 
-if [ -f "$file2" ]
+if [ -f "$fileB" ]
 then
-    echo "removing $file2"
-    rm -i $file2
+    echo "removing $fileB"
+    rm -i $fileB
 fi
 
 ./daqd --socket-name=/tmp/d.sock --daq-type=GBE
