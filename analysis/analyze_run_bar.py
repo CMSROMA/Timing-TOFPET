74e3673ced744b24fbfbfcf6900f9bbad3d70f29
CMS Roma DAQ user <cmsroma.daq@gmail.com>
2019-07-06 00:11:38 +0200

    added crystal ball function of 1274keV pe peak

--- analysis/analyze_run_bar.py
+++ analysis/analyze_run_bar.py
@@ -12,6 +12,28 @@ from array import array
 
 from ROOT import *
 
+def totalFunction(x,par):
+    
+    t = (x[0]-par[4])/par[5]
+    if par[17]<0: 
+        t = -t
+
+    absAlpha = abs(par[17])
+    if( t >= - absAlpha ):
+        crystalball = par[3]*TMath.Exp(-0.5*t*t)
+    else:
+        a = TMath.Power(par[18]/absAlpha,par[18])*TMath.Exp(-0.5*absAlpha*absAlpha)
+        b = par[18]/absAlpha - absAlpha
+        crystalball = par[3]*(a/TMath.Power(b-t,par[18]))
+
+    return par[0]*(1./(1+TMath.Exp(par[1]*(x[0]-par[2])))+crystalball+(1+TMath.Erf((x[0]-par[12])/(par[13]*TMath.Sqrt(x[0]))))*( par[6]/(1+TMath.Exp(par[7]*(x[0]-par[8]))) + par[14]*TMath.Gaus(x[0],par[15],par[16]) )+par[9]*TMath.Gaus(x[0],par[10],par[11]))
+
+
+#def totalFunction(x,par):
+
+#    return par[0]*(1./(1+TMath.Exp(par[1]*(x[0]-par[2])))+par[3]*TMath.Gaus(x[0],par[4],par[5])+(1+TMath.Erf((x[0]-par[12])/(par[13]*TMath.Sqrt(x[0]))))*( par[6]/(1+TMath.Exp(par[7]*(x[0]-par[8]))) + par[14]*TMath.Gaus(x[0],par[15],par[16]) )+par[9]*TMath.Gaus(x[0],par[10],par[11]))
+
+
 ###############
 ## Pixel+Bar ##
 ###############
@@ -172,10 +194,92 @@ for event in range (0,treeSingles.GetEntries()):
             histos_Singles[ch].Fill(treeSingles.energy-mean_PedTot[ch])
 
 ################################################
+## 4) Fits to energy spectrum 
+################################################
+
+
+minEnergy = 3
+maxEnergy = 150
+
+peak=histos_Singles[channels[0]].GetBinCenter(histos_Singles[channels[0]].GetMaximumBin())
+norm=float(histos_Singles[channels[0]].GetEntries())/histos_Singles[channels[0]].GetNbinsX()
+histos_Singles[channels[0]].GetXaxis().SetRangeUser(minEnergy,maxEnergy)
+
+#fTot=TF1("fTot","[0]*(1./(1+TMath::Exp([1]*(x-[2])))+[3]*TMath::Gaus(x,[4],[5])+(1+TMath::Erf((x-[12])/([13]*TMath::Sqrt(x))))*([6]/(1+TMath::Exp([7]*(x-[8])))+[14]*TMath::Gaus(x,[15],[16]))+[9]*TMath::Gaus(x,[10],[11]))",minEnergy,maxEnergy)
+
+fTot = TF1("fTot",totalFunction,minEnergy,maxEnergy,19)
+
+fTot.SetNpx(100000)
+
+##Normalisation
+fTot.SetParameter(0,norm)
+#fTot.SetParLimits(0,norm*0.1,norm*3)
+
+##1274 KeV compton 
+fTot.SetParameter(1,0.003)
+#fTot.SetParLimits(1,0.0001,0.01)
+fTot.SetParameter(2,1.9*peak)
+fTot.SetParLimits(2,1.8*peak,2.0*peak)
+
+##1274 KeV "photo-electric" 
+fTot.SetParameter(3,1.)
+fTot.SetParLimits(3,0.2,5.)
+fTot.SetParameter(4,2.1*peak)
+fTot.SetParLimits(4,2.0*peak,2.2*peak)
+fTot.SetParameter(5,0.1*peak)
+fTot.SetParLimits(5,0.03*peak,0.2*peak)
+fTot.SetParameter(17,0.4)
+fTot.SetParLimits(17,0.1,2.)
+fTot.SetParameter(18,2.)
+fTot.SetParLimits(18,0.1,5.)
+
+##511 KeV compton
+fTot.SetParameter(6,20.)
+#fTot.SetParLimits(6,1,50.)
+fTot.SetParameter(7,0.003)
+#fTot.SetParLimits(7,0.0001,0.005)
+fTot.SetParameter(8,0.6*peak)
+fTot.SetParLimits(8,0.05*peak,1.3*peak)
+
+##Trigger turn on (Compton+BS)
+fTot.SetParameter(12,5.)
+fTot.SetParLimits(12,0,10.)
+fTot.SetParameter(13,5.)
+fTot.SetParLimits(13,0.1,10.)
+
+##511 KeV photoelectric
+fTot.SetParameter(9,15.)
+#fTot.SetParLimits(9,5,50.)
+fTot.SetParameter(10,peak)
+fTot.SetParLimits(10,0.9*peak,1.1*peak)
+fTot.SetParameter(11,0.05*peak)
+fTot.SetParLimits(11,0.02*peak,0.2*peak)
+
+##Back scatter
+fTot.SetParameter(14,0.01)
+fTot.SetParLimits(14,0.,10.)
+fTot.SetParameter(15,0.45*peak)
+fTot.SetParLimits(15,0.3*peak,0.6*peak)
+fTot.SetParameter(16,0.07*peak)
+fTot.SetParLimits(16,0.04*peak,0.13*peak)
+
+#R.gStyle.SetOptTitle(0)
+#R.gStyle.SetOptStat(0)
+#R.gStyle.SetOptFit(1111111)
+#R.gStyle.SetStatH(0.09)
+
+canvas=TCanvas("c","c",900,700)
+histos_Singles[channels[0]].Draw("PE")
+histos_Singles[channels[0]].Fit("fTot","LR+0","",minEnergy,min(peak*2.5,maxEnergy))
+histos_Singles[channels[0]].Fit("fTot","LR+0","",minEnergy,min(peak*2.5,maxEnergy))
+histos_Singles[channels[0]].Fit("fTot","LR+0","",minEnergy,min(peak*2.5,maxEnergy))
+histos_Singles[channels[0]].Fit("fTot","LR+","",minEnergy,min(peak*2.5,maxEnergy))
+
+################################################
 ## X) Output file
 ################################################
 
-tfileoutput = TFile( "output.root", 'recreate' )
+tfileoutput = TFile( "output_Run"+run+".root", "recreate" )
 tfileoutput.cd()
 
 for ch in channels:
@@ -189,6 +293,8 @@ for ch in channels:
     print "PedestalTot "+str(mean_PedTot[ch])+" "+str(rms_PedTot[ch]) 
     #singles
     histos_Singles[ch].Write()
+
+canvas.Write()
 
 tfileoutput.Close()
 
