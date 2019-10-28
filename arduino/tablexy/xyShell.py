a48eaac33f3e84f2153da78db47a335413a088d1
Paolo Meridiani <paolo.meridiani@cern.ch>
2019-10-28 10:46:40 +0100

    untested changes to use float numbers

--- arduino/tablexy/xyShell.py
+++ arduino/tablexy/xyShell.py
@@ -17,38 +17,38 @@ class XYControlPanel(npyscreen.ActionForm):
         # Add the TitleText widget to the form
         self.port = self.add(npyscreen.TitleFilename, name="PORT     :", value="8820", editable=False)
         self.xyMover = XYMover(int(self.port.value))
-        xHome=int(self.xyMover.estimatedPosition().split(" ")[0])
-        yHome=int(self.xyMover.estimatedPosition().split(" ")[1])
-        self.xpos = self.add(npyscreen.TitleText, name="X_POS       :", value="%d"%xHome, rely=4)
+        xHome=round(float(self.xyMover.estimatedPosition().split(" ")[0]),1)
+        yHome=round(float(self.xyMover.estimatedPosition().split(" ")[1]),1)
+        self.xpos = self.add(npyscreen.TitleText, name="X_POS       :", value="%3.1f"%xHome, rely=4)
         self.xposSlider  = self.add(npyscreen.TitleSlider, out_of=48, name = "X_POS       :", value=xHome, editable=False)
-        self.ypos = self.add(npyscreen.TitleText, name="Y_POS       :", value="%d"%yHome, rely=7)
+        self.ypos = self.add(npyscreen.TitleText, name="Y_POS       :", value="%3.1f"%yHome, rely=7)
         self.yposSlider  = self.add(npyscreen.TitleSlider, out_of=48, name = "Y_POS       :", value=yHome, editable=False)
 
 
     def on_ok(self):
         (xpos,ypos)=self.xyMover.estimatedPosition().split(" ")
         try:
-            if (int(xpos) != int(self.xpos.value) or 
-                int(ypos) != int(self.ypos.value)):
+            if (round(float(xpos),1) != round(float(self.xpos.value),1) or 
+                round(float(ypos),1) != round(float(self.ypos.value),1)):
                 xpos=self.xpos.value
                 ypos=self.ypos.value
                 self.xpos.value="Moving"
                 self.ypos.value="Moving"
                 self.xpos.display()
                 self.ypos.display()
-                self.xyMover.moveAbsoluteXY(int(xpos),int(ypos))
+                self.xyMover.moveAbsoluteXY(round(float(xpos),1),round(float(ypos),1))
                 time.sleep(0.5)
                 (xpos,ypos)=self.xyMover.estimatedPosition().split(" ")
                 self.xpos.value=xpos
                 self.ypos.value=ypos
-                self.xposSlider.value=int(xpos)
+                self.xposSlider.value=round(float(xpos),1)
                 self.xpos.display()
                 self.ypos.display()
-                self.yposSlider.value=int(ypos)
+                self.yposSlider.value=round(float(ypos),1)
 
         except ValueError as ve:
-            self.xpos.value = "Invalid coordinates. Please use an integer number"
-            self.ypos.value = "Invalid coordinates. Please use an integer number"
+            self.xpos.value = "Invalid coordinates. Please use a float number XX.X"
+            self.ypos.value = "Invalid coordinates. Please use a float number YY.Y"
             self.xpos.display()
             self.ypos.display()
         except:
