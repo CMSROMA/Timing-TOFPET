ec923b13b7df7515e73324cfc8106e955f4f5bcb
CMS Roma DAQ user <cmsroma.daq@gmail.com>
2019-12-11 15:34:51 +0100

    added data for irradiated arrays

--- analysis/barScanPlots.py
+++ analysis/barScanPlots.py
@@ -5,9 +5,12 @@ from array import array
 import math as mt
 import numpy as np
 
-data =[ "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000655_LastRun000690_BAR000027.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000583_LastRun000618_BAR000044.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000619_LastRun000654_BAR000059.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000691_LastRun000726_BAR000095.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000727_LastRun000762_BAR000081.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000799_LastRun000834_BAR000111.root" , "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000835_LastRun000870_BAR000165.root" ,"/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000871_LastRun000906_BAR000125.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000907_LastRun000942_BAR000150.root"]
+#data =[ "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000655_LastRun000690_BAR000027.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000583_LastRun000618_BAR000044.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000619_LastRun000654_BAR000059.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000691_LastRun000726_BAR000095.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000727_LastRun000762_BAR000081.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000799_LastRun000834_BAR000111.root" , "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000835_LastRun000870_BAR000165.root" ,"/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000871_LastRun000906_BAR000125.root", "/media/cmsdaq/ext/data/LYSOBARS/RESULTS/tree_FirstRun000907_LastRun000942_BAR000150.root"]
 
-thrValues = [5,10,15,20,25,30,35,40,45,50,55,60]
+data =[ "/media/cmsdaq/ext/data/LYSOBARSINARRAY/RESULTS/tree_FirstRun000040_LastRun000057_BAR001762.root" , "/media/cmsdaq/ext/data/LYSOBARSINARRAY/RESULTS/tree_FirstRun000058_LastRun000075_BAR001822.root", "/media/cmsdaq/ext/data/LYSOBARSINARRAY/RESULTS/tree_FirstRun000076_LastRun000093_BAR001792.root" , "/media/cmsdaq/ext/data/LYSOBARSINARRAY/RESULTS/tree_FirstRun000094_LastRun000111_BAR001772.root" , "/media/cmsdaq/ext/data/LYSOBARSINARRAY/RESULTS/tree_FirstRun000112_LastRun000129_BAR001802.root", "/media/cmsdaq/ext/data/LYSOBARSINARRAY/RESULTS/tree_FirstRun000130_LastRun000147_BAR001782.root" , "/media/cmsdaq/ext/data/LYSOBARSINARRAY/RESULTS/tree_FirstRun000148_LastRun000165_BAR001812.root" , "/media/cmsdaq/ext/data/LYSOBARSINARRAY/RESULTS/tree_FirstRun000166_LastRun000183_BAR001832.root" , "/media/cmsdaq/ext/data/LYSOBARSINARRAY/RESULTS/tree_FirstRun000184_LastRun000201_BAR001842.root"]
+
+#thrValues = [5,10,15,20,25,30,35,40,45,50,55,60]
+thrValues = [5,10,15,20,25,30]
     
 c1=R.TCanvas("c1","c1",800,800)
     
@@ -18,10 +21,10 @@ slopeByProd=R.TGraphErrors(len(data))
 threshByProd=R.TGraphErrors(len(data))
 
 for ip,prod in enumerate(data):
-    #print prod
+    print ip, prod
 
     firstRun = int(prod.split("/")[-1].split("_")[1].strip("FirstRun").lstrip("0"))
-    #print firstRun
+    print firstRun
 
     thisFile = R.TFile(prod)
     thisFile.Print()
@@ -39,7 +42,7 @@ for ip,prod in enumerate(data):
 
     g_th1_vs_time[ip].SetMarkerStyle(20)
     g_th1_vs_time[ip].SetMarkerColor(R.kBlack+ip)
-    #g_th1_vs_time[ip].Print()
+    g_th1_vs_time[ip].Print()
     if ip==0:
         g_th1_vs_time[ip].Draw("ap")
     else:
@@ -47,7 +50,7 @@ for ip,prod in enumerate(data):
 
     xL,yL,xR,yR=R.Double(0.),R.Double(0.),R.Double(0.),R.Double(0.)
     g_th1_vs_time[ip].GetPoint(0,xL,yL)
-    g_th1_vs_time[ip].GetPoint(6,xR,yR)
+    g_th1_vs_time[ip].GetPoint(5,xR,yR)
     g_th1_vs_time[ip].Fit("pol1","RQ","",xL,xR)
     g_th1_vs_time[ip].GetFunction("pol1").SetLineColor(R.kBlack+ip)
 
@@ -62,10 +65,9 @@ for ip,prod in enumerate(data):
 
     threshByProd.SetPoint(ip,ip+1,yL)
     threshByProd.SetPointError(ip,0,g_th1_vs_time[ip].GetErrorY(0))
-c1.SaveAs("plot.png")
+c1.SaveAs("TimeVsAmpl.png")
 
 R.gStyle.SetOptFit(111111)
-
 
 gCorr.Draw("AP*")
 gCorr.Fit('pol1',"FEX0")
