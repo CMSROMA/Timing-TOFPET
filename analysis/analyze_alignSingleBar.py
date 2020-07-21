5f1471a7631344bc31c46ec9ca5dfb834739f3f1
CMS Roma DAQ user <cmsroma.daq@gmail.com>
2020-07-21 13:05:06 +0200

    alignment of single bar and single bar on the reference side

---
+++ analysis/analyze_alignSingleBar.py
@@ -0,0 +1,142 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import optparse
+import datetime
+import subprocess
+from glob import glob
+from collections import defaultdict
+from collections import OrderedDict
+from array import array
+
+from ROOT import *
+
+#--------------------------------------------------------
+
+usage = "usage: python analysis/analyze_alignArray.py -i /media/cmsdaq/ext/data/ALIGNMENT/ALIGNBAR_14_02_2020 -o /media/cmsdaq/ext/data/ALIGNMENT/ALIGNBAR_14_02_2020"
+
+parser = optparse.OptionParser(usage)
+
+parser.add_option("-i", "--input", dest="inputDir",
+                  help="input directory")
+
+parser.add_option("-o", "--output", dest="outputDir",
+                  help="output directory")
+
+(opt, args) = parser.parse_args()
+
+if not opt.inputDir:   
+    parser.error('input directory not provided')
+
+if not opt.outputDir:   
+    parser.error('output directory not provided')
+
+#--------------------------------------------------------
+
+gROOT.SetBatch(True)
+
+gStyle.SetOptTitle(0)
+gStyle.SetOptStat("e")
+gStyle.SetOptFit(1111111)
+gStyle.SetStatH(0.09)
+
+#--------------------------------------------------------
+
+list_allfiles = os.listdir(opt.inputDir)
+#print list_allfiles
+
+histoY = TH1F("histoY","histoY",510,-0.5,50.5)
+histoX = TH1F("histoX","histoX",510,-0.5,50.5)
+
+for file in list_allfiles:
+
+    if ("_coincidences.root" in file):
+        input_filename_coinc = opt.inputDir + "/" + file
+        print input_filename_coinc
+
+        X = float(input_filename_coinc.split("/")[-1].split("_")[4].replace("X",""))
+        Y = float(input_filename_coinc.split("/")[-1].split("_")[5].replace("Y",""))
+        POS = int(input_filename_coinc.split("/")[-1].split("_")[3].replace("POS",""))
+        print X, Y, POS    
+
+        tfile = TFile.Open(input_filename_coinc)
+        tree = tfile.Get("data")
+        #tree.Print()
+
+        nhits_bar = 0 
+        nhits_coinc = 0 
+        for event in range (0,tree.GetEntries()):
+            tree.GetEntry(event)
+                    
+            if(tree.energy[1]>-9 and tree.energy[2]>-9):
+                nhits_bar = nhits_bar + 1
+
+            if(tree.energy[0]>-9 and tree.energy[1]>-9 and tree.energy[2]>-9):
+                nhits_coinc = nhits_coinc + 1
+
+        print nhits_bar, nhits_coinc
+
+        #scan X
+        if (POS >= 21 and POS <= 33):
+            histoX.SetBinContent(histoX.GetXaxis().FindBin(X),nhits_bar)
+            histoX.SetBinError(histoX.GetXaxis().FindBin(X),sqrt(nhits_bar))
+
+        #scan Y
+        if (POS >= 0 and POS <= 20):
+            histoY.SetBinContent(histoY.GetXaxis().FindBin(Y),nhits_coinc)
+            histoY.SetBinError(histoY.GetXaxis().FindBin(Y),sqrt(nhits_coinc))
+
+#fit
+binxmax = histoY.GetBinCenter(histoY.GetMaximumBin()) 
+print "binxmax: ", binxmax
+
+func_fity = TF1("func_fity","gaus",binxmax-3,binxmax+3)
+fity = histoY.Fit("func_fity","SR")
+#func_fitx = histoX.GetFunction("gaus")
+
+fitx = histoX.Fit("pol2","S")
+func_fitx = histoX.GetFunction("pol2")
+
+#maximum
+max_X = func_fitx.GetX(func_fitx.GetMaximum())
+max_Y = func_fity.GetX(func_fity.GetMaximum())
+print "================================="
+print "==== maxX, maxY: " , max_X, max_Y
+print "================================="
+
+#style
+histoX.GetXaxis().SetTitle("X [mm]")
+histoX.GetYaxis().SetTitle("Number of bar hits")
+histoX.SetMarkerStyle(20)
+histoX.SetMarkerSize(0.7)
+
+histoY.GetXaxis().SetTitle("Y [mm]")
+histoY.GetYaxis().SetTitle("Number of pixel+bar coincidence hits")
+histoY.SetMarkerStyle(20)
+histoY.SetMarkerSize(0.7)
+
+text1=TLatex()
+text1.SetTextSize(0.04)
+
+#plots
+c1 = TCanvas("c2","",500,500)
+histoY.Draw("pe")
+histoY.GetYaxis().SetLimits(histoY.GetMinimum()*0.8,histoY.GetMaximum()*1.2)
+histoY.GetYaxis().SetRangeUser(histoY.GetMinimum()*0.8,histoY.GetMaximum()*1.2)
+c1.SetGridx()
+c1.SetGridy()
+text1.DrawLatexNDC(0.11,0.93,"Y_{max} = %.1f mm" % max_Y)
+
+c2 = TCanvas("c3","",500,500)
+histoX.Draw("pe")
+histoX.GetYaxis().SetLimits(histoX.GetMinimum()*0.8,histoX.GetMaximum()*1.2)
+histoX.GetYaxis().SetRangeUser(histoX.GetMinimum()*0.8,histoX.GetMaximum()*1.2)
+c2.SetGridx()
+c2.SetGridy()
+text1.DrawLatexNDC(0.11,0.93,"X_{max} = %.1f mm" % max_X)
+
+c1.SaveAs(opt.outputDir+"/"+"alignArray_Y.png")
+c1.SaveAs(opt.outputDir+"/"+"alignArray_Y.root")
+c2.SaveAs(opt.outputDir+"/"+"alignArray_X.png")
+c2.SaveAs(opt.outputDir+"/"+"alignArray_X.root")
---
+++ run_DAQ_alignSingleBar.py
@@ -0,0 +1,189 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import optparse
+import datetime
+import subprocess
+from glob import glob
+from collections import defaultdict
+from collections import OrderedDict
+from array import array
+import time
+import re
+
+sys.path.insert(1, os.path.join(sys.path[0], './arduino/tablexy'))
+from xyMover import XYMover
+
+usage = "usage: run from Timing-TOFPET: \n python run_DAQ_alignSingleBar.py -c config_main_bar.txt -o /media/cmsdaq/ext/data/ALIGNMENT/ALIGNBAR_14_02_2020 -n ALIGNBAR" 
+parser = optparse.OptionParser(usage)
+parser.add_option("-c", "--config", dest="configFile",
+                  help="config file")
+parser.add_option("-o", "--outFolder", dest="outputFolder",
+                  help="output directory")
+parser.add_option("--pedAllChannels", dest="pedAllChannels", default=0, 
+                  help="Set to 1 to collect pedestals for all channels (default is 0)")
+parser.add_option("-n", "--name", dest="nameLabel",
+                  help="label for output files")
+(opt, args) = parser.parse_args()
+if not opt.configFile:   
+    parser.error('config file not provided')
+if not opt.outputFolder:   
+    parser.error('output folder not provided')
+if not opt.nameLabel:   
+    parser.error('label for output files not provided')
+
+#############################
+
+commandOutputDir = "mkdir -p "+opt.outputFolder
+print commandOutputDir
+os.system(commandOutputDir)
+
+#############################
+## Daq setup
+#############################
+def RUN(runtype,time,ov,ovref,gate,label,enabledCh="",thresholds="",thresholdsT1=""):
+
+    ###############
+    ## Current time
+    ###############
+    current_time = datetime.datetime.now()
+    simpletimeMarker = "%04d-%02d-%02d-%02d-%02d-%02d" % (current_time.year,current_time.month,current_time.day,current_time.hour,current_time.minute,current_time.second)
+    print simpletimeMarker
+
+    ####################
+    ## Update run number
+    ####################
+    currentRun = 0
+    outputFileName = opt.outputFolder+"/RunNumbers.txt"
+    file_runs = open(outputFileName, 'a+')
+    
+    lastRun = subprocess.check_output(['tail', '-1', outputFileName])
+    lastRun = lastRun.rstrip('\n')
+    
+    if not lastRun:
+        currentRun = 1
+    else:
+        currentRun = int(lastRun) + 1
+    file_runs.write(str(currentRun)+'\n')    
+    file_runs.close()
+
+    #################
+    ## Write commands
+    #################
+    newlabel = "Run"+str(currentRun).zfill(6)+"_"+simpletimeMarker+"_"+label
+
+    if(runtype == "PED"):
+        commandRun = "python run_TOFPET.py -c "+ opt.configFile+" --runType PED -d acquire_pedestal_data " + "-t "+ str(time)+" -v "+str(ov)+" --ovref "+str(ovref)+" -l "+str(newlabel)+" -g "+str(gate)+" -o "+opt.outputFolder+" --pedAllChannels " + str(opt.pedAllChannels)
+        if (enabledCh!=""):
+            commandRun = commandRun +" --enabledChannels " + str(enabledCh) 
+
+    if(runtype == "PHYS"):
+        commandRun = "python run_TOFPET.py -c "+ opt.configFile+" --runType PHYS -d my_acquire_sipm_data " + "-t "+ str(time)+" -v "+str(ov)+" --ovref "+str(ovref)+" -l "+str(newlabel)+" -g "+str(gate)+" -o "+opt.outputFolder
+        if (enabledCh!=""):
+            commandRun = commandRun +" --enabledChannels " + str(enabledCh) 
+            if (thresholds!=""):
+                commandRun = commandRun + " --energyThr " + str(thresholds)
+            if (thresholdsT1!=""):
+                commandRun = commandRun + " --energyThrT1 " + str(thresholdsT1)
+
+    print commandRun
+    os.system(commandRun)
+
+    return;
+
+###################
+## Run daq sequence
+###################
+t_ped = 0.3 #s
+t_phys = 10 #s
+ov_values = [7] #V
+ovref_values = [7] #V
+gate_values = [15] # DeltaT[ns]/20: gate=15 -> DeltaT=300 ns 
+name = opt.nameLabel
+
+nseq = 1
+
+#--------------------------------------------------------------------
+
+########################
+#Position scan for bar in array
+########################
+
+#Reference Bar 
+posRefX = 24.9 
+posRefY = 24.0
+
+dict_PosScan = {
+
+    ##ALIGN BAR Y
+    0: (round(posRefX,1),round(posRefY-12.0,1),"0_1_2","0_0_0","20_10_10"),
+    1: (round(posRefX,1),round(posRefY-8.0,1),"0_1_2","0_0_0","20_10_10"),
+    2: (round(posRefX,1),round(posRefY-4.0,1),"0_1_2","0_0_0","20_10_10"),
+    3: (round(posRefX,1),round(posRefY-3.5,1),"0_1_2","0_0_0","20_10_10"),
+    4: (round(posRefX,1),round(posRefY-3.0,1),"0_1_2","0_0_0","20_10_10"),
+    5: (round(posRefX,1),round(posRefY-2.5,1),"0_1_2","0_0_0","20_10_10"),
+    6: (round(posRefX,1),round(posRefY-2.0,1),"0_1_2","0_0_0","20_10_10"),
+    7: (round(posRefX,1),round(posRefY-1.5,1),"0_1_2","0_0_0","20_10_10"),
+    8: (round(posRefX,1),round(posRefY-1.0,1),"0_1_2","0_0_0","20_10_10"),
+    9: (round(posRefX,1),round(posRefY-0.5,1),"0_1_2","0_0_0","20_10_10"),
+    10: (round(posRefX,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    11: (round(posRefX,1),round(posRefY+0.5,1),"0_1_2","0_0_0","20_10_10"),
+    12: (round(posRefX,1),round(posRefY+1.0,1),"0_1_2","0_0_0","20_10_10"),
+    13: (round(posRefX,1),round(posRefY+1.5,1),"0_1_2","0_0_0","20_10_10"),
+    14: (round(posRefX,1),round(posRefY+2.0,1),"0_1_2","0_0_0","20_10_10"),
+    15: (round(posRefX,1),round(posRefY+2.5,1),"0_1_2","0_0_0","20_10_10"),
+    16: (round(posRefX,1),round(posRefY+3.0,1),"0_1_2","0_0_0","20_10_10"),
+    17: (round(posRefX,1),round(posRefY+3.5,1),"0_1_2","0_0_0","20_10_10"),
+    18: (round(posRefX,1),round(posRefY+4.0,1),"0_1_2","0_0_0","20_10_10"),
+    19: (round(posRefX,1),round(posRefY+8.0,1),"0_1_2","0_0_0","20_10_10"),
+    20: (round(posRefX,1),round(posRefY+12.0,1),"0_1_2","0_0_0","20_10_10"),
+
+    ##ALIGN BAR X
+    21: (round(posRefX-15.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    22: (round(posRefX-12.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    23: (round(posRefX-9.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    24: (round(posRefX-6.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    25: (round(posRefX-4.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    26: (round(posRefX-2.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    27: (round(posRefX,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    28: (round(posRefX+2.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    29: (round(posRefX+4.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    30: (round(posRefX+6.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    31: (round(posRefX+9.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    32: (round(posRefX+12.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10"),
+    33: (round(posRefX+15.0,1),round(posRefY,1),"0_1_2","0_0_0","20_10_10")
+
+}
+
+print "Position scan" , dict_PosScan
+
+###################################################################
+########################### Run DAQ ############################### 
+###################################################################
+
+aMover=XYMover(8820) # 8820 = motor_0 = bar or array or second pixel
+print (aMover.estimatedPosition())
+
+for seq in range(0,nseq):
+    for ov in ov_values:
+        for ovref in ovref_values:
+            for gate in gate_values:
+                for posStep, posInfo in dict_PosScan.items():
+
+                    print "++++ Centering Bar: "+str(posStep)+": X="+str(posInfo[0])+" Y="+str(posInfo[1])+" Channels="+str(posInfo[2])+" +++++"
+                    print aMover.moveAbsoluteXY(posInfo[0],posInfo[1])
+                    if (aMover.moveAbsoluteXY(posInfo[0],posInfo[1]) is "error"):
+                        print "== Out of range: skipping this position =="
+                        continue
+                    print aMover.estimatedPosition()
+                    print "++++ Done +++++"                    
+
+                    thisname = name+"_POS"+str(posStep)+"_X"+str(posInfo[0])+"_Y"+str(posInfo[1])+"_CH"+str(posInfo[2]).replace("_","-")+"_ETHR"+str(posInfo[3]).replace("_","-")+"_T1THR"+str(posInfo[4]).replace("_","-")
+
+                    #============================================
+                    #RUN("PED",t_ped,ov,ovref,gate,thisname,posInfo[2],"","")
+                    RUN("PHYS",t_phys,ov,ovref,gate,thisname,posInfo[2],posInfo[3],posInfo[4]) 
+                    #RUN("PED",t_ped,ov,ovref,gate,thisname,posInfo[2],"","")
+                    #============================================
+
